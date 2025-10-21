752e307490cbdc503b58160a528212ffb7d3cd27
Felipe <felipedasilva23785@gmail.com>
2025-10-21 09:24:14 -0300

    fix(authController): Corrige AuthController para gerar token e retornar usuário logado corretamente

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,11 +5,24 @@ namespace App\Http\Controllers;
 use App\Constants\Geral;
 use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function login(LoginRequest $request)
     {
+        $credentials = $request->only('email', 'password');
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Credenciais inválidas.'
+            ], 401);
+        }
+
+        $user = $request->user();
+
+        $token = $user->createToken('auth-token')->plainTextToken;
 
         return response()->json([
             'status' => true,
